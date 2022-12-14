a206ce0ce0b93c838f6c5c06394ffb04c5b92833
Tung Nguyen <pinenguyen1608@gmail.com>
2022-12-14 21:31:16 +0700

    14-12-2022

---
+++ 1802/go.mod
@@ -0,0 +1,3 @@
+module leetcode-problems/1802
+
+go 1.18
---
+++ 1802/main.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+func check(n, index, val, maxSum int) bool {
+	left := index + 1
+	right := n - 1 - index
+	var leftSum, rightSum int
+	if left >= val {
+		leftSum = left - val + val*(val+1)/2
+	} else {
+		start := val - left + 1
+		leftSum = val*(val+1)/2 - start*(start+1)/2 + start
+	}
+
+	val = val - 1
+	if right >= val {
+		rightSum = right - val + val*(val+1)/2
+	} else {
+		end := val - right + 1
+		rightSum = val*(val+1)/2 - end*(end+1)/2 + end
+	}
+	return leftSum+rightSum <= maxSum
+}
+
+func maxValue(n int, index int, maxSum int) int {
+	r := int(1e9) + 1
+	l := 1
+	for l < r {
+		mid := (l + r) / 2
+		if check(n, index, mid, maxSum) {
+			l = mid + 1
+		} else {
+			r = mid
+		}
+	}
+	return l - 1
+}
+
+func main() {
+	fmt.Println(maxValue(4, 2, 6))
+	fmt.Println(maxValue(6, 1, 10))
+}
